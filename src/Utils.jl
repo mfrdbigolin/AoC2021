0469b71a2a7f9efca2f13f901c967854bfb2eb44
Matheus Fernandes Bigolin <mfrdrbigolin@disroot.org>
2021-12-25 12:13:18 -0300

    Ninth day

--- src/Utils.jl
+++ src/Utils.jl
@@ -8,7 +8,7 @@
 
 module Utils
 
-export arrange, usage_and_exit, bin2dec, ∑, array2num
+export arrange, usage_and_exit, bin2dec, ∑, array2num, find_neighbors
 
 arrange(vs :: Vector, dtype = String) = [parse(dtype, v) for v in vs]
 
@@ -27,5 +27,30 @@ bin2dec(B :: AbstractArray) = sum((*).(B, (^).(2, (length(B) - 1):-1:0)))
 
 "Transform an array <N> of digits in a number."
 array2num(N :: AbstractArray) = N .* (^).(10, (length(N) - 1):-1:0) |> sum
+
+"Find the neighbors (non-diagonal) of <(i, j)> in the matrix <A>."
+function find_neighbors(A :: AbstractMatrix, (i, j) :: Tuple{Int, Int})
+    (m, n) = size(A)
+
+    neighbors = Array{Tuple{Int, Int}}([])
+
+    if i != 1
+        push!(neighbors, (i - 1, j))
+    end
+
+    if i != m
+        push!(neighbors, (i + 1, j))
+    end
+
+    if j != 1
+        push!(neighbors, (i, j - 1))
+    end
+
+    if j != n
+        push!(neighbors, (i, j + 1))
+    end
+
+    return neighbors
+end
 
 end
---
+++ src/day09.jl
@@ -0,0 +1,80 @@
+#!/usr/bin/julia
+
+# Copyright (C) 2021 Matheus Fernandes Bigolin <mfrdrbigolin@disroot.org>
+# SPDX-License-Identifier: MIT
+
+"Day Nine, Smoke Basin."
+
+
+#= At first I left this day uncompleted because I lacked the graph theory
+ = knowledge to solve part two; but after learning the Dijkstra’s algorithm for
+ = day fifteen, I managed to solve this problem in a similar way.
+ =#
+
+include("Utils.jl")
+
+using .Utils
+
+"Calculate the sum of the risk levels of all low points in <heightmap>."
+function solve1(heightmap :: Matrix{Int})
+    risklevel_count = 0
+
+    for ind in CartesianIndices(heightmap)
+        neighbors = find_neighbors(heightmap, ind.I)
+
+        if heightmap[ind] < minimum([heightmap[n...] for n in neighbors])
+            risklevel_count += heightmap[ind] + 1
+        end
+    end
+
+    return risklevel_count
+end
+
+"Calculate the product of the size of the three largest basins in <heightmap>."
+function solve2(heightmap :: Matrix{Int})
+    # Mark all locations with height nine as visited.
+    unvisited = BitArray{2}([row .!= 9 for row in heightmap])
+
+    current = (1, 1)
+    next = Array{Tuple{Int, Int}}([])
+
+    basins = Array{Int}([])
+    current_basin = 0
+
+    while count(unvisited) != 0
+        unvisited[current...] = false
+        current_basin += 1
+
+        neighbors = find_neighbors(heightmap, current)
+
+        filter!(x -> unvisited[x...] && x ∉ next, neighbors)
+        push!(next, neighbors...)
+
+        if isempty(next)
+            push!(basins, current_basin)
+            current_basin = 0
+
+            if count(unvisited) != 0
+                current = findfirst(unvisited).I
+            end
+        else
+            current = pop!(next)
+        end
+    end
+
+    return prod(sort(basins)[end - 2:end])
+end
+
+function main()
+    usage_and_exit(length(ARGS) != 1)
+
+    data = collect.(readlines(first(ARGS)))
+    heightmap = hcat([parse.(Int, l) for l in data]...)
+
+    println(solve1(heightmap))
+    println(solve2(heightmap))
+end
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    main()
+end
